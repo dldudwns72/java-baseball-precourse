dc7e8fdf77a84e2272c4c8b8e4c414044711e223
dldudwns72 <dldudwns72@gmail.com>
2022-04-16 13:20:36 +0900

    feat(judegement) : 두개의 공을 판별하여 결과값을 리턴하는 judegement 도메인 구현

--- src/main/java/baseball/domain/Judgement.java
+++ src/main/java/baseball/domain/Judgement.java
@@ -1,2 +1,29 @@
-package baseball.domain;public class Comparision {
+package baseball.domain;
+
+public class Judgement {
+
+    private Ball computerBall;
+    private Ball userBall;
+
+    public Judgement(Ball computerBall, Ball userBall) {
+        this.computerBall = computerBall;
+        this.userBall = userBall;
+    }
+
+    public String getResult(){
+        if(computerBall.getNumber() == userBall.getNumber()){
+           return judgeStrikeOrBall();
+        }
+
+        return "볼";
+
+    }
+
+    private String judgeStrikeOrBall(){
+        if(computerBall.getPosition() == userBall.getPosition()){
+            return "스트라이크";
+        }
+        return "볼";
+    }
+
 }
