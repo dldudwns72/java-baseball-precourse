b5952b486c8002b7e0f06d6a671782d577f56319
dldudwns72 <dldudwns72@gmail.com>
2022-04-16 18:36:17 +0900

    feat : 세자리 공과 하나의 공을 판별하여 결과값 반환

--- src/main/java/baseball/domain/Judgement.java
+++ src/main/java/baseball/domain/Judgement.java
@@ -2,28 +2,40 @@ package baseball.domain;
 
 public class Judgement {
 
-    private Ball computerBall;
+    private Balls computerBalls;
     private Ball userBall;
 
-    public Judgement(Ball computerBall, Ball userBall) {
-        this.computerBall = computerBall;
+    public Judgement(Balls computerBalls, Ball userBall) {
+        this.computerBalls = computerBalls;
         this.userBall = userBall;
     }
 
-    public String getResult(){
-        if(computerBall.getNumber() == userBall.getNumber()){
-           return judgeStrikeOrBall();
+    public String getResult() {
+        for (Ball computerBall : computerBalls.getBalls()) {
+            return checkBall(computerBall);
         }
-
-        return "볼";
-
+        return "낫싱";
     }
 
-    private String judgeStrikeOrBall(){
-        if(computerBall.getPosition() == userBall.getPosition()){
+    private String checkBall(Ball computerBall) {
+        if (isEqualNumber(computerBall)) {
+            return checkBallPosition(computerBall);
+        }
+        return "볼";
+    }
+
+    private String checkBallPosition (Ball computerBall) {
+        if (isEqualPosition(computerBall)) {
             return "스트라이크";
         }
         return "볼";
     }
 
+    private boolean isEqualNumber(Ball computerBall){
+        return computerBall.getNumber() == userBall.getNumber();
+    }
+
+    private boolean isEqualPosition(Ball computerBall){
+        return computerBall.getPosition() == userBall.getPosition();
+    }
 }
