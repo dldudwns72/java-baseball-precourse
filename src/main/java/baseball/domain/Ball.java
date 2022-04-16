5312fbd2b2a7c1f9554ee8f81d92db33b2203ae7
dldudwns72 <dldudwns72@gmail.com>
2022-04-16 10:03:05 +0900

    feat(Balls) : 야구 게임 공 생성

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -1,2 +1,40 @@
-package baseball.domain;public class Ball {
+package baseball.domain;
+
+public class Ball {
+
+    private final String BALL_POSITION_EXCEPTION_MESSAGE = "공 위치의 범위는 1~3입니다.";
+    private final String BALL_NUMBER_EXCEPTION_MESSAGE = "공 숫자의 범위는 0~9 사이 입니다.";
+
+    private int position;
+
+    private int number;
+
+    public Ball(int position, int number){
+        if(isOverPosition(position)) throw new IllegalArgumentException(BALL_POSITION_EXCEPTION_MESSAGE);
+        if(isOverNumber(number) ) throw new IllegalArgumentException(BALL_NUMBER_EXCEPTION_MESSAGE);
+        this.position =position;
+        this.number = number;
+    }
+
+    private boolean isOverPosition(int position){
+        return position < 0 && position > 3;
+    }
+
+    private boolean isOverNumber(int number){
+        return number < 0 && number > 9;
+    }
+
+    public int getNumberByPosition(int position){
+        return number;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+
 }
--- src/main/java/baseball/domain/Balls.java
+++ src/main/java/baseball/domain/Balls.java
@@ -1,2 +1,48 @@
-package baseball.domain;public class Balls {
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Balls {
+
+    private final String BALL_DUPLICATION_EXCEPTION_MESSAGE = "공의 숫자는 중복될 수 없습니다.";
+
+    private List<Ball> balls = new ArrayList<>();
+    private List<Integer> ballNumbers = new ArrayList<>();
+
+    public Balls(List<Ball> balls) {
+        ballNumbers = getBallNumbers(balls);
+        List<Integer> prevBallNumbers = new ArrayList<>();
+        for(Ball ball : balls){
+            checkDuplicatedBall(prevBallNumbers,ball);
+            prevBallNumbers.add(ball.getNumber());
+        }
+        this.balls = balls;
+    }
+
+    private List<Integer> getBallNumbers(List<Ball> balls) {
+        List<Integer> ballNumbers = new ArrayList<>();
+        for (Ball ball : balls) {
+            ballNumbers.add(ball.getNumber());
+        }
+        return ballNumbers;
+    }
+
+    private void checkDuplicatedBall(List<Integer> prevBallNumbers, Ball ball){
+        if(prevBallNumbers.contains(ball.getNumber())){
+            throw new IllegalArgumentException(BALL_DUPLICATION_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public Ball getBall(int index){
+        return balls.get(index);
+    }
+
+    public List<Ball> getBalls() {
+        return balls;
+    }
+
+    public int getSize(){
+        return balls.size();
+    }
 }
