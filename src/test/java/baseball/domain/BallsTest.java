2430c81b93ffcce4ae8cc4d959ece86f592f9a26
dldudwns72 <dldudwns72@gmail.com>
2022-04-16 10:03:25 +0900

    test : Balls 테스트

--- src/test/java/baseball/domain/BallsTest.java
+++ src/test/java/baseball/domain/BallsTest.java
@@ -2,14 +2,47 @@ package baseball.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
 
-class BallTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BallsTest {
 
     @Test
     @DisplayName("공 생성")
     public void createBall(){
-        Ball ball = new Ball(1,1);x
+        Ball ball = new Ball(1,1);
+        assertThat(ball.getPosition()).isEqualTo(1);
+        assertThat(ball.getNumber()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @DisplayName("세자리 공 생성")
+    @CsvSource(value = {"0:1","1:2","2:6"} ,delimiter = ':')
+    public void createBalls(int index , int ballNumber){
+        Ball ball1 = new Ball(1,1);
+        Ball ball2 = new Ball(2,2);
+        Ball ball3 = new Ball(3,6);
+        Balls balls = new Balls(Arrays.asList(ball1,ball2,ball3));
+
+        Ball ball = balls.getBall(index);
+        assertThat(ball.getNumberByPosition(index+1)).isEqualTo(ballNumber);
+    }
+
+    @Test
+    @DisplayName("중복 발생 시 IllegalArgumentException 예외 발생")
+    public void duplicatedBallsException(){
+        Ball ball1 = new Ball(1,1);
+        Ball ball2 = new Ball(2,1);
+        Ball ball3 = new Ball(3,6);
+
+        assertThatThrownBy(() ->{
+            Balls balls = new Balls(Arrays.asList(ball1,ball2,ball3));
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("공의 숫자는 중복될 수 없습니다.");
     }
 }
