ecc143d41663db37caf37c6f19fc56a2b6c1cd28
dldudwns72 <dldudwns72@gmail.com>
2022-04-18 02:07:27 +0900

    test : Ball 내부에서 Strike,Ball 판별 및 예외 발생 테스트

--- src/test/java/baseball/domain/BallTest.java
+++ src/test/java/baseball/domain/BallTest.java
@@ -1,2 +1,71 @@
-package baseball.domain;public class BallTest {
+package baseball.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BallTest {
+
+    Ball answer;
+
+    Ball userBall1;
+    Ball userBall2;
+    Ball userBall3;
+
+    @BeforeEach
+    void setUp(){
+         answer = new Ball(1,1);
+
+         userBall1 = new Ball(1,1);
+         userBall2 = new Ball(1,4);
+         userBall3 = new Ball(2,1);
+    }
+
+    @Test
+    @DisplayName("Strike 일 경우")
+    void isStrike() {
+        assertTrue(answer.isStrike(userBall1));
+    }
+
+    @Test
+    @DisplayName("Strike 가 아닐 경우")
+    void isNotStrike() {
+        assertFalse(answer.isStrike(userBall2));
+    }
+
+    @Test
+    @DisplayName("Ball 일 경우")
+    void isBall() {
+        assertTrue(answer.isBall(userBall3));
+    }
+
+    @Test
+    @DisplayName("Ball 이 아닐 경우")
+    void isNotBall() {
+        assertFalse(answer.isBall(userBall2));
+    }
+
+    @Test
+    @DisplayName("공의 숫자가 범위를 벗어날 경우 에러 발생")
+    void generateNumberException(){
+        assertThatThrownBy(() ->{
+            new Ball(1,11);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("범위는 1~9 사이");
+    }
+
+    @Test
+    @DisplayName("공의 숫자가 범위를 벗어날 경우 에러 발생")
+    void generatePositionException(){
+        assertThatThrownBy(() ->{
+            new Ball(4,3);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("공은 세자리수");
+
+    }
+
 }
