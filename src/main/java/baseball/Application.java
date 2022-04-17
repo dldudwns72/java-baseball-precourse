563aa85913a1261abcd4ea42f59e627b5c4fb880
dldudwns72 <dldudwns72@gmail.com>
2022-04-18 02:45:45 +0900

    feat(UI) : 정답을 맞춘 후 게임 재시작 및 종료 여부 체크

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import baseball.ui.Game;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int tryNumber = 1;
+
+        while (tryNumber == 1) {
+            new Game().start();
+            String restart = Console.readLine();
+            tryNumber = Integer.parseInt(restart);
+        }
     }
+
+
 }
