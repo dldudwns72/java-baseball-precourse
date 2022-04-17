c213de67fdf6920ecaf07ec84e0d89290d17547e
dldudwns72 <dldudwns72@gmail.com>
2022-04-18 02:09:01 +0900

    chore(Balls) : 사용하지 않는 메서드 제거

--- src/main/java/baseball/domain/Balls.java
+++ src/main/java/baseball/domain/Balls.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,33 +9,23 @@ public class Balls {
     private final String BALL_DUPLICATION_EXCEPTION_MESSAGE = "공의 숫자는 중복될 수 없습니다.";
 
     private List<Ball> balls = new ArrayList<>();
-    private List<Integer> ballNumbers = new ArrayList<>();
 
     public Balls(List<Ball> balls) {
-        ballNumbers = getBallNumbers(balls);
         List<Integer> prevBallNumbers = new ArrayList<>();
-        for(Ball ball : balls){
-            checkDuplicatedBall(prevBallNumbers,ball);
+        for (Ball ball : balls) {
+            checkDuplicatedBall(prevBallNumbers, ball);
             prevBallNumbers.add(ball.getNumber());
         }
         this.balls = balls;
     }
 
-    private List<Integer> getBallNumbers(List<Ball> balls) {
-        List<Integer> ballNumbers = new ArrayList<>();
-        for (Ball ball : balls) {
-            ballNumbers.add(ball.getNumber());
-        }
-        return ballNumbers;
-    }
-
-    private void checkDuplicatedBall(List<Integer> prevBallNumbers, Ball ball){
-        if(prevBallNumbers.contains(ball.getNumber())){
+    private void checkDuplicatedBall(List<Integer> prevBallNumbers, Ball ball) {
+        if (prevBallNumbers.contains(ball.getNumber())) {
             throw new IllegalArgumentException(BALL_DUPLICATION_EXCEPTION_MESSAGE);
         }
     }
 
-    public Ball getBall(int index){
+    public Ball getBall(int index) {
         return balls.get(index);
     }
 
@@ -42,7 +33,4 @@ public class Balls {
         return balls;
     }
 
-    public int getSize(){
-        return balls.size();
-    }
 }
